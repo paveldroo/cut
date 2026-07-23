ef4279a794973ccccb1ca214891662cd0c214cc0
Pavel Druzhinin <paveldroo@gmail.com>
2026-07-24 00:01:59 +0300

    add -d argument

--- main.go
+++ main.go
@@ -9,19 +9,51 @@ import (
 	"strings"
 )
 
+type Config struct {
+	fname     string
+	fieldIdx  *int
+	delimiter string
+}
+
 func main() {
 	args := os.Args[1:]
 	if len(args) <= 1 {
 		log.Fatal("you should provide arguments")
 	}
 
-	fieldNoStr, ok := strings.CutPrefix(args[0], "-f")
-	if !ok {
-		log.Fatalf("no -f argument was found in %s", args[0])
+	cfg := Config{
+		fname:     "",
+		fieldIdx:  nil,
+		delimiter: "	", // tab by default
 	}
-	fieldNo, err := strconv.Atoi(fieldNoStr)
-	if err != nil {
-		log.Fatalf("string conversion of %s to integer", fieldNoStr)
+
+	for i, arg := range args {
+		if i == len(args)-1 {
+			cfg.fname = arg
+
+			continue
+		}
+
+		if fieldNoStr, ok := strings.CutPrefix(arg, "-f"); ok {
+			fieldNo, err := strconv.Atoi(fieldNoStr)
+			if err != nil {
+				log.Fatalf("string conversion of %s to integer", fieldNoStr)
+			}
+			fieldIdx := fieldNo - 1
+			cfg.fieldIdx = &fieldIdx
+
+			continue
+		}
+
+		if delimiter, ok := strings.CutPrefix(arg, "-d"); ok {
+			cfg.delimiter = delimiter
+
+			continue
+		}
+	}
+
+	if cfg.fieldIdx == nil {
+		log.Fatalf("no -f argument was found in args: %v", args)
 	}
 
 	fname := args[len(args)-1]
@@ -34,8 +66,19 @@ func main() {
 
 	for s.Scan() {
 		line := s.Text()
-		splits := strings.Split(line, "	")
-		fmt.Printf("%v\n", splits[fieldNo-1])
-		// fmt.Printf("%s\n", line)
+		splits := strings.Split(line, cfg.delimiter)
+		if len(splits) == 1 {
+			fmt.Printf("%v\n", splits[0])
+
+			continue
+		}
+
+		if len(splits) < *cfg.fieldIdx+1 {
+			fmt.Println("")
+
+			continue
+		}
+
+		fmt.Printf("%v\n", splits[*cfg.fieldIdx])
 	}
 }
